19170c4c5505186cd3bc6e5b62ffa45e4a9b86fe
DanieZFinger <dzfinge@g.clemson.edu>
2022-12-11 20:09:26 -0500

    page outline creation

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,15 +1,34 @@
 import './App.css';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import image from "./images/tents2.jpg"; 
+import { useNavigate } from "react";
+import { Link } from 'react'
+// import {useEffect, useState} from 'react';
 
 function App() {
-  return (
+    // let navigate = useNavigate();
+    // const routeChangeGitHub = () =>{ 
+    //   let path = `https://github.com/DanielZFinger`; 
+    //   navigate(path);
+    // }
+    // const routeChangeLinkedIn = () =>{ 
+    //   let path = `https://www.linkedin.com/in/daniel-finger/`; 
+    //   navigate(path);
+    // }
+    return(
     <div className="App">
       <header className="App-header">
-        <Box>
-          <img src={image} width="100%" />
-          Daniel Finger
+        <Box style={{alignItems: 'start', justifyContent: 'start', flex:1}}>
+          <span className="font-link">
+            <Typography sx={{mt: "10%",fontSize:"80%"}}><p>Hey, my name is</p></Typography>
+          </span>
+          <Typography sx={{fontSize:"100%", color:"orange"}}><h1>Daniel Finger</h1></Typography>
+
+          <Typography sx={{mt: "30%",fontSize:"100%", color:"orange"}}><h4>Skills</h4></Typography>
+          <Typography className="spacing" sx={{width: "100%",fontSize:"60%", color:"orange"}}><h4>C | C++ | C# | Python | Java | JavaScript | CSS | HTML</h4></Typography>
+          <Button component={Link} to="https://github.com/DanielZFinger" sx={{mt:"10%", fontSize:"100%",color:"orange"}} >GitHub</Button>
+          <Button sx={{mt:"10%", fontSize:"100%",color:"orange"}} >LinkedIn</Button>
         </Box>
       </header>
     </div>
